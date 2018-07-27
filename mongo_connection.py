0a1e87d2b53bc1c4c56c0663769a04c623084623
Andy Walsh <andyjasonwalsh@gmail.com>
2018-07-27 12:49:33 -0500

    Added indexes construct to the collection return

--- mongo_connection.py
+++ mongo_connection.py
@@ -3,8 +3,8 @@ Project: MigrationSourceValidator
 File: mongo_connection.py
 Created: 2018-07-25T14:04:08.539Z
 WrittenBy: anwalsh
-Last Modified: 2018-07-25T20:42:31.305Z
-Revision: 1.0
+Last Modified: 2018-07-27T17:49:07.603Z
+Revision: 1.16
 Description: Handles connecting to the MongoDB Host and associated fault tolerance.
 """
 from pprint import pprint
@@ -16,19 +16,22 @@ def MakeConnection(c):
 def GetNamespaces(c):
     namespaces = dict((db, [coll for coll in GenGetCollStats(c, db)])
                         for db in c.database_names() if db not in ('admin', 'local', 'config'))
+
     pprint(namespaces, indent = 4)
 
-    for cstat in GenGetCollStats(c, 'yelp'):
-        pprint(cstat, indent = 4)
-    
 def GenGetDBStats(c, db):
-    yield c[db].command({'dbstats' : 1, "size" : 1024})
+    yield c[db].command({'dbstats' : 1, 'scale' : 1024})
 
 def GenGetCollStats(c, db):
     for coll in c[db].collection_names():
-        data = c.yelp.command('collstats', coll)
+        data = c[db].command('collstats', coll, scale=1024)
         target = {data.get('ns'): [{'count': data.get('count'), 'size': data.get('size'),
                                     'avgObjSize': data.get('avgObjSize'), 'capped': data.get('capped'),
                                     'nindexes': data.get('nindexes'), 'totalIndexSize': data.get('totalIndexSize'),
-                                    'indexSizes': data.get('indexSizes')}]}
+                                    'indexSizes': data.get('indexSizes')}, {'Indexes': GetIndexes(c, db, coll)}]}
         yield target
+
+def GetIndexes(c, db, coll):
+    indexes = c[db][coll].index_information()
+
+    return indexes
